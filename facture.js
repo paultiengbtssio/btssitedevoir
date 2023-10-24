900b16c79b5c84e6cfd337f0f546157642f22ea9
paultiengbtssio <144143173+paultiengbtssio@users.noreply.github.com>
2023-10-24 14:38:11 +0200

    Add files via upload

--- facture.js
+++ facture.js
@@ -1,17 +1,17 @@
 function Facture() {
     var a =document.getElementById("D").value;
     var b =document.getElementById("E").value;
-    var c = Number(a)+Number(b);
+    var c = Number(a)*Number(b);
     t3.value=(c)
 
     var d =document.getElementById("G").value;
     var e =document.getElementById("H").value;
-    var f = Number(d)+Number(e);
+    var f = Number(d)*Number(e);
     t45.value=(f)
 
     var h =document.getElementById("J").value;
     var j =document.getElementById("K").value;
-    var k = Number(h)+Number(j);
+    var k = Number(h)*Number(j);
     L.value=(k)
 
     var somme = Number(k)+Number(c)+Number(f);
